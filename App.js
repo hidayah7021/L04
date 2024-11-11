c4eeaf48c7f4995eed17e22b9b19b14a6ba87fcb
hidayah7021 <22007021@myrp.edu.sg>
2024-11-11 14:46:12 +0800

    up

--- App.js
+++ App.js
@@ -1,20 +1,324 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// // ======= Exercise1 =======
+// import React from 'react';
+// import {View, Text} from 'react-native';
+//
+// const Exercise1 = () => {
+//   return (
+//       <View style={{marginTop: 50, margin: 20}}>
+//         <Text style={{fontSize: 24}}>
+//           RP Values
+//         </Text>
+//         <Text style={{color: 'green'}}>
+//           Excellence
+//         </Text>
+//         <Text style={{backgroundColor: 'yellow'}}>
+//           Customer-centric
+//         </Text>
+//         <Text style={{fontStyle: 'italic'}}>
+//           Intelligence
+//         </Text>
+//         <Text style={{textAlign: 'center'}}>
+//           Teamwork
+//         </Text>
+//         <Text style={{backgroundColor: 'black', color: 'white'}}>
+//           Enterprising
+//         </Text>
+//       </View>
+//   )
+// }
+//
+// export default Exercise1;
 
-export default function App() {
+// // ====== Exercise2 =======
+// import React from 'react';
+// import {View, Text, StyleSheet} from 'react-native';
+//
+// const styles = StyleSheet.create({
+//   greenBox:{
+//     width: 100,
+//     height: 100,
+//     marginTop: 30,
+//     backgroundColor: 'green',
+//     borderWidth: 1,
+//     borderColor: 'black',
+//   },
+//   boxText:{
+//     textAlign: 'center',
+//     color: 'white',
+//   },
+//   title: {
+//     fontWeight: 'bold'
+//   }
+// });
+//
+// const Exercise2 = () => {
+//   return (
+//       <View style={{marginTop: 50, margin:20}}>
+//         <View style={styles.greenBox}>
+//           <Text style={[styles.boxText, styles.title]}>
+//             Who We Are
+//           </Text>
+//         </View>
+//         <View style={styles.greenBox}>
+//           <Text style={styles.boxText}>
+//             Our Campus
+//           </Text>
+//         </View>
+//         <View style={styles.greenBox}>
+//           <Text style={styles.boxText}>
+//             Our People
+//           </Text>
+//         </View>
+//       </View>
+//   )
+// }
+//
+// export default Exercise2;
+
+// // // ====== Exercise3A =======
+// import React from 'react';
+// import {View, Text, StyleSheet} from 'react-native';
+//
+// const styles2 = StyleSheet.create({
+//   parent:{
+//     marginTop:50,
+//     margin:20,
+//     flexDirection: 'row',
+//     backgroundColor:'#F5fcff',
+//     borderColor: '#0099AA',
+//     borderWidth: 5
+//   },
+//   child:{
+//     flexDirection: 'row',
+//     borderWidth: 2,
+//     textAlign:'center',
+//     fontSize: 27
+//   }
+// });
+//
+// const Exercise3A = () => {
+//   return (
+//       <View style={styles2.parent}>
+//         <Text style={[styles2.child, {backgroundColor: 'powderblue'}]}>
+//           ChildOne
+//         </Text>
+//         <Text style={[styles2.child, {backgroundColor: 'skyblue'}]}>
+//           ChildTwo
+//         </Text>
+//         <Text style={[styles2.child, {backgroundColor: 'steelblue'}]}>
+//           ChildThree
+//         </Text>
+//       </View>
+//   )
+// }
+//
+// export default Exercise3A;
+
+// // // ====== Exercise3B =======
+// import React from 'react';
+// import {View, Text, StyleSheet} from 'react-native';
+//
+// const styles2 = StyleSheet.create({
+//   parent:{
+//     flex:1,
+//     marginTop:50,
+//     margin:20,
+//     flexDirection: 'column',
+//     backgroundColor:'#F5fcff',
+//     borderColor: '#0099AA',
+//     borderWidth: 5
+//   },
+//   child:{
+//     flex:1,
+//     flexDirection: 'row',
+//     borderWidth: 2,
+//     textAlign:'center',
+//     fontSize: 27
+//   }
+// });
+//
+// const Exercise3B = () => {
+//   return (
+//       <View style={styles2.parent}>
+//         <Text style={[styles2.child, {backgroundColor: 'powderblue'}]}>
+//           ChildOne
+//         </Text>
+//         <Text style={[styles2.child, {backgroundColor: 'skyblue'}]}>
+//           ChildTwo
+//         </Text>
+//         <Text style={[styles2.child, {backgroundColor: 'steelblue'}]}>
+//           ChildThree
+//         </Text>
+//       </View>
+//   )
+// }
+//
+// export default Exercise3B;
+
+// // // ====== Exercise3C =======
+// import React from 'react';
+// import {View, Text, StyleSheet} from 'react-native';
+//
+// const styles2 = StyleSheet.create({
+//   parent:{
+//     flex:1,
+//     marginTop:50,
+//     margin:20,
+//     flexDirection: 'column',
+//     backgroundColor:'#F5fcff',
+//     borderColor: '#0099AA',
+//     borderWidth: 5
+//   },
+//   child:{
+//     flex:1,
+//     flexDirection: 'row',
+//     borderWidth: 2,
+//     textAlign:'center',
+//     fontSize: 20
+//   }
+// });
+//
+// const Exercise3C = () => {
+//   return (
+//       <View style={styles2.parent}>
+//         <Text style={[styles2.child, {backgroundColor: 'powderblue', maxWidth:90}]}>
+//           Child One
+//         </Text>
+//         <Text style={[styles2.child, {backgroundColor: 'skyblue'}]}>
+//           Child Two
+//         </Text>
+//         <Text style={[styles2.child, {backgroundColor: 'steelblue', maxHeight:120}]}>
+//           Child Three
+//         </Text>
+//       </View>
+//   )
+// }
+//
+// export default Exercise3C;
+
+// // ====== Exercise3D =======
+// import React from 'react';
+// import {View, Text, StyleSheet} from 'react-native';
+//
+// const styles2 = StyleSheet.create({
+//   parent:{
+//     marginTop:50,
+//     margin:20,
+//     flexDirection: 'row',
+//     backgroundColor:'#F5fcff',
+//     borderColor: '#0099AA',
+//     borderWidth: 5
+//   },
+//   child:{
+//     flexDirection: 'row',
+//     borderWidth: 2,
+//     textAlign:'center',
+//     fontSize: 20
+//   }
+// });
+//
+// const Exercise3D = () => {
+//   return (
+//       <View style={styles2.parent}>
+//         <Text style={[styles2.child, {backgroundColor: 'powderblue', flex:1 }]}>
+//           ChildOne
+//         </Text>
+//         <Text style={[styles2.child, {backgroundColor: 'skyblue', flex:2}]}>
+//           ChildTwo
+//         </Text>
+//         <Text style={[styles2.child, {backgroundColor: 'steelblue', flex:3}]}>
+//           ChildThree
+//         </Text>
+//       </View>
+//   )
+// }
+//
+// export default Exercise3D;
+
+// // // ====== Exercise3E =======
+// import React from 'react';
+// import {View, Text, StyleSheet} from 'react-native';
+//
+// const styles2 = StyleSheet.create({
+//   parent:{
+//     flex: 1,
+//     marginTop:50,
+//     margin:20,
+//     flexDirection: 'column',
+//     // justifyContent: 'space-around'
+//     // justifyContent: 'flex-end'
+//     // justifyContent: 'flex-start'
+//     justifyContent: 'space-between',
+//     backgroundColor:'#F5fcff',
+//     borderColor: '#0099AA',
+//     borderWidth: 5
+//   },
+//   child:{
+//     flexDirection: 'row',
+//     borderWidth: 2,
+//     textAlign:'center',
+//     fontSize: 20
+//   }
+// });
+//
+// const Exercise3E = () => {
+//   return (
+//       <View style={styles2.parent}>
+//         <Text style={[styles2.child, {backgroundColor: 'powderblue'}]}>
+//           ChildOne
+//         </Text>
+//         <Text style={[styles2.child, {backgroundColor: 'skyblue'}]}>
+//           ChildTwo
+//         </Text>
+//         <Text style={[styles2.child, {backgroundColor: 'steelblue'}]}>
+//           ChildThree
+//         </Text>
+//       </View>
+//   )
+// }
+//
+// export default Exercise3E;
+
+// // // ====== Exercise4 =======
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const styles2 = StyleSheet.create({
+  parent:{
+    flex: 1,
+    marginTop:50,
+    margin:20,
+    flexDirection: 'row',
+    backgroundColor:'smokegrey',
+    borderColor: '#0099AA',
+    borderWidth: 5,
+      justifyContent:'space-evenly',
+      alignItems: 'center'
+  },
+  child:{
+      width: 80,
+      height: 80,
+    flexDirection: 'row',
+    borderWidth: 2,
+    textAlign:'center',
+    fontSize: 20
+  }
+});
+
+const Exercise4 = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+      <View style={styles2.parent}>
+        <Text style={[styles2.child, {backgroundColor: 'red'}]}>
+          Square 1
+        </Text>
+        <Text style={[styles2.child, {backgroundColor: 'yellow'}]}>
+          Square 2
+        </Text>
+        <Text style={[styles2.child, {backgroundColor: 'green'}]}>
+          Square 3
+        </Text>
+      </View>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default Exercise4;
